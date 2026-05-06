c6c9275caeacf7e9a1a003457f4f04d64cb00c69
Christian Boin <christianboin@hotmail.com>
2026-05-05 19:37:58 -0700

    style: format routes.py

--- songbirdapi/routes.py
+++ songbirdapi/routes.py
@@ -93,6 +93,7 @@ PLAYER_QUEUE_REORDER = f"{V1}/player/queue/reorder"
 def player_queue_song_path(song_id: str) -> str:
     return f"{PLAYER_QUEUE}/{song_id}"
 
+
 # Playlists
 PLAYLISTS = f"{V1}/playlists"
 
